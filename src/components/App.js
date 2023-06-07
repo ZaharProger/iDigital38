997ea72fecbbde288fd7a00a8c2c382c09234a6f
ZaharDomolego <domolegozahar@gmail.com>
2023-06-07 22:10:36 +0800

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Routes } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div id="App">
+      <Routes>
+      </Routes>
+    </div>
+  )
+}
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import initialState from './initialState'
+import reducer from './reducer.js'
+
+const store = configureStore({ reducer: reducer, preloadedState: initialState })
+
+export default store;
