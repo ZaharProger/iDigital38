81fc4546427641451ffb612e5ba958166c95ba86
ZaharDomolego <domolegozahar@gmail.com>
2023-06-15 00:48:20 +0800

    Добавил конфиг Webpack

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production'
+}
+
+module.exports = {
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: "./public/index.html"
+            }
+        )
+    ],
+    mode,
+    entry: "./src/index.js",
+    output:{
+        assetModuleFilename: "assets/[name][ext]",
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true
+    },
+    devtool: "source-map",
+    devServer: {
+        historyApiFallback: true,
+        port: 3000,
+        hot: true,
+        open: true
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(jsx|js)$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                options:{
+                    cacheDirectory: true,
+                    presets:[ "@babel/preset-react", "@babel/preset-env"]
+                }
+            },
+            {
+                test: /\.css?$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.(svg|png|eot|ttf|woff|woff2])$/,
+                type: "asset/resource"
+            }
+        ]
+    }
+}
