274cc767ab04f92652bf77c4fd88f9a3c2010b82
Я Яшкин <misha_bog100@inbox.ru>
2023-06-23 00:47:06 +0800

    Подвал сделан, осталось добавить пару ссылок и кнопку нёрпу

--- src/components/content/footer/Footer.js
+++ src/components/content/footer/Footer.js
@@ -42,12 +42,6 @@ export default function Footer(){
                     <img src={vk} id="Icon" style={{marginLeft: 30}}></img>
                 </div>
                 <h2> <a href="https://www.istu.edu/" style={{fontSize: 18, fontWeight: "bold", color: "white", marginLeft: 175}}>ФГБОУ ВО ИРНИТУ</a></h2>
-                {/* <div style={{ display: "flex", fontWeight: "bold", alignItems: "baseline" }}>
-                    <span className="purple-letter" style={{ fontSize: 50, marginBottom: "-10px" }}>i</span>
-                    <span style={{ fontSize: 50, fontFamily: "hisqaida_2018bold", marginBottom: "-10px" }}>D</span>
-                    <span style={{ fontSize: 40, fontFamily: "hisqaida_2018bold" }}>igital</span>
-                    <span className="purple-letter" style={{ fontSize: 50, marginBottom: "-10px",fontFamily: "hisqaida_2018bold"}}>38</span>
-                </div> */}
                 <Logo></Logo>
             </div>
             <div id="Line"></div>
