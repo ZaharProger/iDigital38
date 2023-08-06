92ec734ba53073ddfb10202b729423d22681cb5e
Евгений Афанасьев <evgenij29082002@gmail.com>
2023-08-06 17:46:01 +0800

    тестовый вариант дизайна страницы авторизации

--- src/components/admin/auth/Auth.js
+++ src/components/admin/auth/Auth.js
@@ -1,20 +1,26 @@
 import React from 'react';
-// import '../../../styles/auth.css'
+import '../../../styles/auth.css'
+import logo from "../../../assets/pics/logo.svg"
 
 export default function Auth() {
   return (
     <div id='Auth'>
-      <form>
-        <h3>Авторизация</h3>
+      <form id='form' action='/' method='post'>
+
+        <img src={logo}/>
+
         <div>
           <span>Логин</span>
           <input type="text" />
         </div>
+
         <div>
           <span>Пароль</span>
           <input type="password" />
         </div>
-        <button type="submit">Войти</button>
+
+        <input id='button' type="submit" value="Войти" />
+      
       </form>
     </div>
   )
