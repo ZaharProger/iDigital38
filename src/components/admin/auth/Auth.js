1d9bcf2c4f9bbaccc5fb1094ec417ed4cfe1ad1a
Евгений Афанасьев <evgenij29082002@gmail.com>
2023-08-14 14:17:28 +0800

    Authentication almost done

--- src/components/admin/auth/Auth.js
+++ src/components/admin/auth/Auth.js
@@ -1,13 +1,73 @@
 import React from 'react';
 import '../../../styles/auth.css'
+import { useNavigate } from "react-router-dom"
 import logo from "../../../assets/pics/logo.svg"
+import useApi from "../../../hooks/useApi"
+import useValidation from "../../../hooks/useValidation"
+import { ROUTES } from '../../../globalConstants';
+
 
 export default function Auth() {
+
+  const navigate = useNavigate()
+  const performApiCall = useApi()
+  const validate = useValidation()
+
+  const validateFields = useCallback((fields, form) => {
+    const validationResult = validate(fields, (inputValue) =>
+      !RegExp(/^\s*$/).test(inputValue))
+
+    form.querySelectorAll('input').forEach(input => {
+      input.classList.remove('error')
+
+      if (validationResult.includes(input)) {
+        input.classList.add('error')
+      }
+    })
+
+    return validationResult.length == 0
+  }, [])
+
+  onSubmitting = (e) => {
+
+    e.preventDefault()
+    const form = document.querySelector('form')
+    if (form !== null) {
+
+      const inputsToValidate = Array.from(document.getElementsByTagName('input'))
+      if (validateFields(inputsToValidate, form)) {
+        const requestMethod = 'POST'
+        const requestBody = new FormData(form)
+
+        const submitButton = form.querySelector('.submit-button')
+        const prevButtonText = submitButton.innerText
+        submitButton.innerText = 'Отправка данных...'
+        submitButton.disabled = true
+
+        performApiCall(`${HOST}/api/login/`, requestMethod, requestBody, null)
+          .then(responseData => {
+            submitButton.innerText = prevButtonText
+            submitButton.disabled = false
+
+            if (responseData.status == 200) {
+              navigate(ROUTES.admin)
+            }
+            else {
+              document.getElementById('error-server').style.display = "flex";
+            }
+          })
+      } else {
+        document.getElementById('error-client').style.display = "flex";
+      }
+    }
+
+  }
+
   return (
     <div id='Auth'>
-      <form id='form' action='/' method='post'>
+      <form id='form' method='post' onSubmit={(e) => onSubmitting(e)}>
 
-        <img src={logo}/>
+        <img src={logo} />
 
         <div>
           <span>Логин</span>
@@ -19,8 +79,17 @@ export default function Auth() {
           <input type="password" />
         </div>
 
-        <input id='button' type="submit" value="Войти" />
-      
+        <button className='submit-button' type="submit">Войти</button>
+
+        <div id='error-server' className='error-msg'>
+          <span>Произошла внутренняя ошибка сервера.</span>
+          <span>Пожалуйста, повторите запрос позже</span>
+        </div>
+
+        <div id='error-client' className='error-msg'>
+          <span>Пожалуйста, заполните все поля</span>
+        </div>
+
       </form>
     </div>
   )
