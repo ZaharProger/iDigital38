689dc824c903b59d743f32a41eda4842e043055b
ZaharDomolego <domolegozahar@gmail.com>
2023-09-27 22:37:55 +0800

    Добавил выгрузку отчетности

--- src/components/admin/header/AdminHeader.js
+++ src/components/admin/header/AdminHeader.js
@@ -1,14 +1,30 @@
-import React from "react"
+import React, {useCallback} from "react"
 import {useNavigate} from "react-router-dom"
 import {useSelector} from "react-redux"
 
 import logo from "../../../assets/pics/logo.svg"
 import {ROUTES} from "../../../globalConstants"
 import '../../../styles/admin-header.css'
+import useApi from "../../../hooks/useApi"
 
 export default function AdminHeader(props) {
     const navigate = useNavigate()
     const adminUsername = useSelector(state => state.username)
+
+    const performApiCall = useApi()
+
+    const downloadAppointmentsList = useCallback(() => {
+        performApiCall('/api/appointments', 'GET', null, null).then(responseData => {
+            if (responseData.status == 200) {
+                const downloadRef = document.querySelector('.download')
+                downloadRef.href = URL.createObjectURL(responseData.data.file)
+                downloadRef.download = responseData.data.name
+
+                downloadRef.click()
+                URL.revokeObjectURL(downloadRef.href);
+            }
+        })
+    }, [])
 
     return (
         <div id="Admin-header" className="d-flex flex-row position-fixed navbar w-100 p-3">
@@ -25,6 +41,12 @@ export default function AdminHeader(props) {
                         :
                         null
                 }
+                <button type="button"
+                        onClick={ () => downloadAppointmentsList() }
+                        className="regular-text d-flex text-center ms-auto mt-auto mb-auto pt-1 pb-1 pe-3 ps-3">
+                    Выгрузить список заявок
+                </button>
+                <a className="d-none download"></a>
             </div>
         </div>
     )
